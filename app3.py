2d3601daa814c1068aca588f1619167200433588
Barcanito <barcankar11@outlook.com>
2024-11-19 23:52:39 +0300

    Added first 4 test codes

---
+++ app3.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+
+#img = cv.imread("photos/cat.jpg")
+
+#cv.imshow('Cat',img)
+
+#cv.waitKey(0)
+
+capture=cv.VideoCapture('videos/dog.mp4')
+
+while True:
+    isTrue, frame=capture.read()
+    cv.imshow('video',frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+
